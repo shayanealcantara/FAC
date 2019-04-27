b9e1762f4ec41f99b31ad415ea2b0f2a4cd8a259
Shayane Alcantara <shayaneliebe@gmail.com>
2019-04-27 16:32:49 -0300

    Ajustando erro no valor 0

--- Trabalho 2/trabalho2.asm
+++ Trabalho 2/trabalho2.asm
@@ -1,25 +1,30 @@
 .data
-	invalida: .asciiz "Entrada invalida.\n"
+	numero1: .asciiz "Digite um n√∫mero: \n"
+	mensagem: .asciiz "Entrada invalida.\n"
 
 .text
 
 main:
-	li $v0, 5 # numero do usu·rio
-	syscall		
-	move $a0, $v0 # numero do user para a0
-		
-	blez $a0, invalid # se $a0 <= 0 ent„o exit
+	li   $v0, 4 #print_string
+	la   $a0, numero1
+	syscall
 	
-	#printar $t2 pra ver se t· passando na funÁ„o
-	move $a0, $t0 #mover o numero para printar em $a0
+	li $v0, 5 # numero do usuario
+	syscall		
+	move $a0, $v0 # numero do user para a0	
+	
+	 
+	bltz $a0, invalid # se $a0 <= 0 entao invalida
+	#blez $a0, invalid # se $a0 <= 0 entao invalida
+	
+	#printar $t2 pra ver se ta passando na funcao
 	li $v0, 1 #carrega print_int em $v0
 	syscall
-
+	
 	li $v0, 10
 	syscall
 
 invalid:
 		li $v0, 4
-		la $a0, invalida	
+		la $a0, mensagem	
 		syscall
-	
